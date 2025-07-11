696fbdbde21f2c28d3c760c2cd1cc6057ea28bd2
Lê Văn Toàn <Toan.LV225160@sis.hust.edu.vn>
2025-07-11 15:14:55 +0700

    hello

---
+++ src/test/java/com/example/BasicSpringBoot/BasicSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BasicSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
